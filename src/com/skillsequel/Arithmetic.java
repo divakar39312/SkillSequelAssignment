2f4fb0dcd89a23b78dcaccf4f03e11a36089b772
Divakar <divakar39312@gmail.com>
2022-08-10 21:56:42 +0530

    [Divakar]:File added

---
+++ src/com/skillsequel/Arithmetic.java
@@ -0,0 +1,20 @@
+package com.skillsequel;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		int a=30;
+		int b=40;
+		int c=60;
+		int add=a+b+c;
+		int sub=a-b-c;
+		int div=b/a;
+		int mod=c%a;
+		System.out.println(add);
+		System.out.println(sub);
+		System.out.println(div);
+		System.out.println(mod);
+
+	}
+
+}
---
+++ src/com/skillsequel/CheckIntoString.java
@@ -0,0 +1,12 @@
+package com.skillsequel;
+
+public class CheckIntoString {
+
+	public static void main(String[] args) {
+	int a=100;
+	String b=a;
+	System.out.println(b);//Not possible
+
+	}
+
+}
